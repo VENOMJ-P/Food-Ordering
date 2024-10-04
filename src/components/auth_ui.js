325c771d08139121be82ec86c81dcba6e09e2e5b
VENOMJ-P <jay63837@gmail.com>
2024-10-04 20:13:31 +0530

    Added css for auth btn

--- src/components/auth_ui.js
+++ src/components/auth_ui.js
@@ -3,11 +3,19 @@ import { useState } from "react";
 const Auth_UI = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   return isAuthenticated ? (
-    <button type="button" onClick={() => setIsAuthenticated(false)}>
+    <button
+      type="button"
+      className="authbtn"
+      onClick={() => setIsAuthenticated(false)}
+    >
       Logout
     </button>
   ) : (
-    <button type="button" onClick={() => setIsAuthenticated(true)}>
+    <button
+      type="button"
+      className="authbtn"
+      onClick={() => setIsAuthenticated(true)}
+    >
       Login
     </button>
   );
